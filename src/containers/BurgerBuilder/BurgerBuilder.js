373910b8c2296c67cf49d0a1ae4336731ab1b811
eric capiz <ericcapiz@gmail.com>
2020-07-01 17:45:53 -0500

    burger controls component added with css

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import Aux1 from '../../hoc/Aux1';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 class BurgerBuilder extends Component{
+    state ={
+        ingredients:{
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0,
+        }
+    }
     render(){
         return(
            <Aux1>
-               <Burger />
-               <div>Build Controls</div>
+               <Burger ingredients={this.state.ingredients}/>
+               <BuildControls />
            </Aux1>
         );
     }
